2d68bbfcf374714c92c8fe7ffd29d0a3c162ac73
VICTOR-OLULETI <oluletiv@gmail.com>
2022-11-03 00:00:26 +0100

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,30 +3,37 @@
 This script deploys archive in web server
 """
 import os.path
-from fabric.api import run, put
-
+from fabric.api import *
+import shlex
+import os
 
 def do_deploy(archive_path):
-    """
-    This deploys the archive file in the web server
-    """
+    """ Deploys """
     env.user = 'ubuntu'
-    env.hosts = ['44.210.78.253', '54.160.124.52', 'localhost']
-    if os.path.exists(archive_path) is False:
-        return (False)
+    env.hosts = ['44.210.78.253', '54.160.124.52']
+    if not os.path.exists(archive_path):
+        return False
     try:
-        filename = archive_path.split('/')[-1]
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-        file = filename.split('.')[0]
-        release_path = '/data/web_static/releases/{}/'.format(file)
-        run('mkdir -p /data/web_static/releases/' + file)
-        run('tar -xzf /tmp/{} -C {}'.format(filename, release_path))
-        run('rm /tmp/{}'.format(filename))
-        run('mv {}web_static/* {}'.format(release_path))
-        run('rm -rf {}web_static'.format(release_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(release_path))
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
+        return True
     except:
-        return (False)
-    return (True)
+        return False
