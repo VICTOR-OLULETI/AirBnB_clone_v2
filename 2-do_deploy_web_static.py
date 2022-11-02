83bc39717cf7a296e2ee61de61adf26c79736209
VICTOR-OLULETI <oluletiv@gmail.com>
2022-11-02 23:18:36 +0100

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,20 +5,24 @@ This script deploys archive in web server
 import os.path
 from fabric.api import run, put
 
+env.user = 'ubuntu'
+env.hosts = ['44.210.78.253', '54.160.124.52']
+
 def do_deploy(archive_path):
     """
     This deploys the archive file in the web server
     """
-    env.user = 'ubuntu'
-    env.hosts = ['44.210.78.253', '54.160.124.52']
-    if os.path.isdir(archive_path) is False:
+    if os.path.exists(archive_path) is False:
         return (False)
     filename = archive_path.split('/')[-1]
-    put(archive_path, "/tmp/" + filename)
+    put(archive_path, "/tmp/")
     file = filename.split('.')[0]
+    release_path = '/data/web_static/releases/{}/'.format(file)
     run('mkdir -p /data/web_static/releases/' + file)
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(filename, file))
+    run('tar -xzf /tmp/{} -C {}'.format(filename, release_path))
     run('rm /tmp/{}'.format(filename))
+    run('mv {}web_static/* {}'.format(release_path))
+    run('rm -rf {}web_static'.format(release_path))
     run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(file))
+    run('ln -s {} /data/web_static/current'.format(release_path))
     return (True)
