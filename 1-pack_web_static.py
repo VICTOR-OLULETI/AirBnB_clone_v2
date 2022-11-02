8ea437d4dacc08680cb865ab065362825fff2e02
VICTOR-OLULETI <oluletiv@gmail.com>
2022-11-02 21:38:44 +0100

    pack_web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,9 +2,16 @@
 # this would generate a .tgz file
 from datetime import datetime
 from fabric.operations import run, get
-name = 'web_static' + datetime.now().hour() + datetime.now().minute + datetime.now().second() + '.tgz'
+name = 'web_static' + datetime.now().hour()
++ datetime.now().minute +
+datetime.now().second() + '.tgz'
+
+
 def do_pack():
+    """
+    This function would store all files in web_static in an archive
+    """
     archive_path = "/versions/" + name
     run("mkdir -p /versions/")
-    get(remote_path="/web/static/*", local_path="/versions/"+ name)
+    get(remote_path="/web/static/*", local_path="/versions/" + name)
     return archive_path
