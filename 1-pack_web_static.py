58eed2239566f8fc6f315a0c56b33624291c1f64
VICTOR-OLULETI <oluletiv@gmail.com>
2022-11-02 22:36:08 +0100

    compress static files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,6 +2,7 @@
 """
 this would generate a .tgz file
 """
+import os.path
 from datetime import datetime
 from fabric.api import run, get, local
 
@@ -11,15 +12,17 @@ def do_pack():
     This function would store all files in web_static in an archive
     """
     dt = datetime.utcnow()
-    archive_file = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
+    archive_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
             dt.year,
             dt.month,
             dt.day,
             dt.hour,
             dt.minute,
             dt.second)
-    local("mkdir -p /versions/")
-    pack = local("tar -cvzf {} web_static".format(archive_file))
-    if (pack.failed):
+
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(archive_file)).failed is True:
         return None
     return archive_file
