6d7a1be7a9cb38793d1cba2bdd18be961eb50027
VICTOR-OLULETI <oluletiv@gmail.com>
2022-11-02 23:22:57 +0100

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,24 +5,27 @@ This script deploys archive in web server
 import os.path
 from fabric.api import run, put
 
-env.user = 'ubuntu'
-env.hosts = ['44.210.78.253', '54.160.124.52']
 
 def do_deploy(archive_path):
     """
     This deploys the archive file in the web server
     """
+    env.user = 'ubuntu'
+    env.hosts = ['44.210.78.253', '54.160.124.52']
     if os.path.exists(archive_path) is False:
         return (False)
-    filename = archive_path.split('/')[-1]
-    put(archive_path, "/tmp/")
-    file = filename.split('.')[0]
-    release_path = '/data/web_static/releases/{}/'.format(file)
-    run('mkdir -p /data/web_static/releases/' + file)
-    run('tar -xzf /tmp/{} -C {}'.format(filename, release_path))
-    run('rm /tmp/{}'.format(filename))
-    run('mv {}web_static/* {}'.format(release_path))
-    run('rm -rf {}web_static'.format(release_path))
-    run('rm -rf /data/web_static/current')
-    run('ln -s {} /data/web_static/current'.format(release_path))
+    try:
+        filename = archive_path.split('/')[-1]
+        put(archive_path, "/tmp/")
+        file = filename.split('.')[0]
+        release_path = '/data/web_static/releases/{}/'.format(file)
+        run('mkdir -p /data/web_static/releases/' + file)
+        run('tar -xzf /tmp/{} -C {}'.format(filename, release_path))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {}web_static/* {}'.format(release_path))
+        run('rm -rf {}web_static'.format(release_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(release_path))
+    except:
+        return (False)
     return (True)
