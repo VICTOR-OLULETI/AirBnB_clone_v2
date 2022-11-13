791a6ec921092ddd4a89d084cd8cb14e9db831b8
VICTOR-OLULETI <oluletiv@gmail.com>
2022-11-12 20:30:59 -0800

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,5 +35,5 @@ def do_deploy(archive_path):
         run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except:
+    except ValueError:
         return False
