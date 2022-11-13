0e5ca2e99b65f27d76c0db30e2d2320a0de50526
VICTOR-OLULETI <oluletiv@gmail.com>
2022-11-12 21:33:09 -0800

    clean web static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Deleting archives
+# Deleting archives
 import os
 from fabric.api import *
 
@@ -8,12 +8,10 @@ def do_clean(number=0):
     """Deletes out dated archives"""
     env.hosts = ['44.210.78.253', '54.160.124.52']
     number = 1 if int(number) == 0 else int(number)
-    
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
-	[local("rm ./{}".format(a)) for a in archives]
-
+        [local("rm ./{}".format(a)) for a in archives]
     with cd("/data/web_static/releases"):
 	archives = run("ls -tr").split()
 	archives = [a for a in archives if "web_static_" in a]
