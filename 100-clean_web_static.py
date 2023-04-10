41fb96147205a8a174610ba8a5390e459da53070
VICTOR-OLULETI <oluletiv@gmail.com>
2023-04-10 15:39:52 +0100

    clean up unwanted archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,22 +3,27 @@
 import os
 from fabric.api import *
 
+env.hosts = ['35.153.232.12', '18.234.253.65']
+
 
 def do_clean(number=0):
+    """ Deletes out dated archives.
+    Args:
+    number: number of archives to keep
     """
-	Deletes out dated archives.
-    	Args:
-	number: number of archives to keep
-    """
-    env.hosts = ['44.210.78.253', '54.160.124.52']
-    number = 1 if int(number) == 0 else int(number)
+    env.hosts = ['35.153.232.12', '18.234.253.65']
+    # number = 1 if int(number) == 0 else int(number)
+    if int(number) == 0:
+        number = 1
+    else:
+        number = int(number)
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-	archives = run("ls -tr").split()
-	archives = [a for a in archives if "web_static_" in a]
-	[archives.pop() for i in range(number)]
-	[run("rm -rf ./{}".format(a)) for a in archives]
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [sudo("rm -rf ./{}".format(a)) for a in archives]
