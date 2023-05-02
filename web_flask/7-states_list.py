f7cfc472d6624640cba2016af60860de9ad0850a
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-02 01:49:25 +0100

    states in alphabetical order

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,8 @@
 # import sys
 # sys.path.append('~/AirBnB_clone_v2/models')
 from flask import Flask, render_template
+from sqlalchemy import text
+from models import *
 from models import storage
 # from models.state import State
 app = Flask(__name__)
@@ -10,13 +12,15 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def stateslist():
-    all_states = storage.all("State").values()
+    all_states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return (render_template('7-states_list.html', all_states=all_states))
+
 
 @app.teardown_appcontext
 def cleanup(exception):
     """ Closes the session """
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port='5000')
